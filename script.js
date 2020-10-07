bc44a042ccffcc524b6ea1f405052c7ec46bed30
Ehfaz <ehfaz.rezwan@gmail.com>
2020-10-08 02:11:20 +0600

    Added text formatting functions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /**
  ̶*̶ ̶T̶O̶D̶O̶:̶ ̶U̶p̶d̶a̶t̶e̶ ̶t̶h̶e̶ ̶t̶e̶x̶t̶ ̶i̶n̶ ̶t̶h̶e̶ ̶"̶F̶o̶r̶m̶a̶t̶t̶e̶d̶ ̶T̶e̶x̶t̶"̶ ̶s̶e̶c̶t̶i̶o̶n̶ ̶a̶s̶ ̶a̶ ̶u̶s̶e̶r̶ ̶t̶y̶p̶e̶s̶ ̶i̶n̶ ̶t̶h̶e̶ ̶t̶e̶x̶t̶a̶r̶e̶a̶
- * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
- * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
+ ̶*̶ ̶T̶O̶D̶O̶ ̶T̶O̶G̶E̶T̶H̶E̶R̶:̶ ̶A̶d̶d̶ ̶a̶ ̶.̶b̶o̶l̶d̶,̶ ̶.̶i̶t̶a̶l̶i̶c̶ ̶c̶l̶a̶s̶s̶e̶s̶ ̶t̶o̶ ̶"̶F̶o̶r̶m̶a̶t̶t̶e̶d̶ ̶T̶e̶x̶t̶"̶ ̶w̶h̶e̶n̶ ̶t̶h̶e̶ ̶a̶p̶p̶r̶o̶p̶r̶i̶a̶t̶e̶ ̶b̶u̶t̶t̶o̶n̶ ̶i̶s̶ ̶c̶l̶i̶c̶k̶e̶d̶
+̶ ̶*̶ ̶T̶O̶D̶O̶:̶ ̶A̶d̶d̶ ̶a̶n̶ ̶.̶u̶n̶d̶e̶r̶l̶i̶n̶e̶ ̶c̶l̶a̶s̶s̶ ̶t̶o̶ ̶"̶F̶o̶r̶m̶a̶t̶t̶e̶d̶ ̶T̶e̶x̶t̶"̶ ̶w̶h̶e̶n̶ ̶U̶n̶d̶e̶r̶l̶i̶n̶e̶ ̶b̶u̶t̶t̶o̶n̶ ̶i̶s̶ ̶c̶l̶i̶c̶k̶e̶d̶
+̶ * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
 
 /**
@@ -27,12 +27,19 @@ function updateText() {
  */
 function makeBold(elem) {
   //CODE GOES HERE
+  let outputBox = document.getElementById("text-output");
+  elem.classList.toggle("active");
+  outputBox.classList.toggle("bold");
 }
 
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem) {}
+function makeItalic(elem) {
+  let outputBox = document.getElementById("text-output");
+  elem.classList.toggle("active");
+  outputBox.classList.toggle("italic");
+}
 
 /**
  * Toggle the underline class for the output text
@@ -42,6 +49,9 @@ function makeItalic(elem) {}
  */
 function makeUnderline(elem) {
   //CODE GOES HERE
+  let outputBox = document.getElementById("text-output");
+  elem.classList.toggle("active");
+  outputBox.classList.toggle("underline");
 }
 
 /**
